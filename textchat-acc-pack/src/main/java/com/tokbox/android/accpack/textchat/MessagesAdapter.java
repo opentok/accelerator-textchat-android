f2da5bc2917de72ff09155fe812b716cf9731dce
Marina Serrano Montes <mserranomontes@MacBook-Pro-de-Marina-2.local>
2020-03-17 10:12:27 +0100

    Update Android deps versions

--- textchat-acc-pack/src/main/java/com/tokbox/android/accpack/textchat/MessagesAdapter.java
+++ textchat-acc-pack/src/main/java/com/tokbox/android/accpack/textchat/MessagesAdapter.java
@@ -1,12 +1,10 @@
 package com.tokbox.android.accpack.textchat;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import com.tokbox.android.accpack.textchat.R;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
